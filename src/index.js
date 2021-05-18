02081eb60359dcf9d3a7d9b09fd4ead7882605c0
Rosa <rosamahmud5@gmail.com>
2021-05-19 01:48:19 +0600

    created a class for my h1 tag

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function formatName (user){
-  return user.firstName + " " + user.lastName;
-}
-const user ={
- firstName : 'Rosa',
- lastName : 'Mahmud'
-};
 
 const element = (
-   <h1>
-   Hello, {formatName(user)}! 
+   <h1 className = 'greetings'>
+   Hello World! 
    </h1>
 );
 
