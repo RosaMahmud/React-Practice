68e6873fc67c9ecbbf22480af398e41bb4819e39
Rosa <rosamahmud5@gmail.com>
2021-05-19 23:41:06 +0600

    tried implementing the tick function

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
 
 
-const element = (
-   <h1 className = 'greetings'>
-   Hello World! 
-   </h1>
-);
+function tick(){
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  reactDom.render(element, document.getElementById('root'));
+}
 
-ReactDOM.render(
-  <React.StrictMode>
- {element}, 
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
