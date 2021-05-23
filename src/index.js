1b73cb40f7b5b48f47c158368432e4e7a23ed4ca
Rosa <rosamahmud5@gmail.com>
2021-05-23 20:26:25 +0600

    Tick function using clock component

--- src/index.js
+++ src/index.js
@@ -9,9 +9,22 @@ import Welcome from './Welcome';
 import Element from './Element';
 
 
-ReactDOM.render(
- <Element/>, document.getElementById('root')
-); 
+function Clock (props){
+    return(
+        <div>
+            <h1>Hello, World!</h1>
+            <h2>It is {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+function tick(){
+    ReactDOM.render(
+        <Clock date = {new Date()} />,
+        document.getElementById('root')
+    );
+
+}
+setInterval(tick,1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
